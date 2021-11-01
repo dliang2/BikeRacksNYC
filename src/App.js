8869e410780e0b4567d912342754e88db6bc58fd
Darren Liang <liangdarren2@gmail.com>
2021-10-31 20:41:11 -0400

    commented to make code more digestable

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Icon } from "leaflet";
 
 //import BikeRackData from "./data/BicycleParking.json";
-import BikeRackData from "./data/dummyData.json";
+import BikeRackData from "./data/dummyData.json"; // Using smaller data because main data takes too long to load
 
 import "./App.css";
 
@@ -16,6 +16,7 @@ function App() {
   // const [longitude, setLongitude] = useState(0);
 
   return (
+    // Generate a world map centered on NYC
     <MapContainer center={[40.7128, -74.006]} zoom={10}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -23,23 +24,29 @@ function App() {
      <a href="http://osm.org/copyright">OpenStreetMap</a> 
      contributors'
       />
-      {BikeRackData.data.map((bikerack) => (
-        <Marker
-          key={bikerack.id}
-          position={[
-            bikerack[8].split(" ")[2].slice(0, -1), // LATITUDE
-            bikerack[8].split(" ")[1].slice(1), // LONGITUDE
-          ]}
-          icon={
-            new Icon({
-              iconUrl:
-                "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-              iconSize: [25, 41],
-              iconAnchor: [12, 41],
-            })
-          }
-        ></Marker>
-      ))}
+
+      {BikeRackData.data.map(
+        (
+          bikerack // Read through the data in the JSON file
+        ) => (
+          <Marker // Display every Bike Rack in the data
+            key={bikerack.id}
+            position={[
+              bikerack[8].split(" ")[2].slice(0, -1), // get LATITUDE
+              bikerack[8].split(" ")[1].slice(1), // get LONGITUDE
+            ]}
+            icon={
+              // Set image of the marker icon
+              new Icon({
+                iconUrl:
+                  "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+              })
+            }
+          ></Marker>
+        )
+      )}
     </MapContainer>
   );
 }
