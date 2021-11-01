654c4c34de94449cadd76a165ec119700e44370a
Darren Liang <liangdarren2@gmail.com>
2021-10-31 20:22:29 -0400

    plotted location of bike racks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from "./logo.svg";
-import react from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useState, useEffect, useContext } from "react";
+
+import "leaflet/dist/leaflet.css";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
+
+//import BikeRackData from "./data/BicycleParking.json";
+import BikeRackData from "./data/dummyData.json";
+
 import "./App.css";
 
 function App() {
+  // const [latitude, setLatitude] = useState(0);
+  // const [longitude, setLongitude] = useState(0);
+
   return (
     <MapContainer center={[40.7128, -74.006]} zoom={10}>
       <TileLayer
@@ -12,6 +23,23 @@ function App() {
      <a href="http://osm.org/copyright">OpenStreetMap</a> 
      contributors'
       />
+      {BikeRackData.data.map((bikerack) => (
+        <Marker
+          key={bikerack.id}
+          position={[
+            bikerack[8].split(" ")[2].slice(0, -1), // LATITUDE
+            bikerack[8].split(" ")[1].slice(1), // LONGITUDE
+          ]}
+          icon={
+            new Icon({
+              iconUrl:
+                "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+            })
+          }
+        ></Marker>
+      ))}
     </MapContainer>
   );
 }
