44a9cb18162b98411e44d087806b4c13a89687b7
SukhiM <44445119+SukhiM@users.noreply.github.com>
2021-11-14 17:17:01 -0500

    Add files via upload
    Added capacity information into marker popup and formatted in popup to display after name of station

---
+++ App.js
@@ -0,0 +1,81 @@
+import logo from "./logo.svg";
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import "leaflet/dist/leaflet.css";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { latLngBounds, Icon } from "leaflet";
+
+import "./App.css";
+
+function App() {
+  // const [latitude, setLatitude] = useState(0);
+  // const [longitude, setLongitude] = useState(0);
+  // const bounds = latLngBounds([40.0341, -74.2727], [41.2919, -71.9101]);
+
+  const [data, setData] = useState([]); // Store data in here
+
+  useEffect(() => {
+    axios
+      .get("https://gbfs.citibikenyc.com/gbfs/en/station_information.json") // Pull the data
+      .then((res) => setData(res.data.data)) // Set the data
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    // Generate a world map centered on NYC
+    <MapContainer
+      center={[40.7128, -74.006]}
+      zoom={11}
+      maxZoom={18}
+      style={{ width: "100%", height: "900px" }}
+      // fillColor="grey"
+    >
+      <TileLayer
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png" // Grey Basemap color
+        attribution='
+        &copy; <a href="https://stadiamaps.com/">Stadia Maps</a> 
+        &copy; <a href="https://openmaptiles.org/">Open Map Tiles</a> 
+        &copy; <a href="https://www.openstreetmap.org/about/">OpenStreetMap contributors</a> 
+     ' // copyright stuff
+        // bounds={bounds}
+      />
+      {data.stations?.map(
+        (
+          station // Read through the data in the JSON file
+        ) => (
+          <Marker // Display every Bike Rack in the data
+            key={station.id}
+            position={[
+              station.lat, // get LATITUDE
+              station.lon, // get LONGITUDE
+            ]}
+            icon={
+              // Set image of the marker icon
+              new Icon({
+                iconUrl:
+                  "https://www.freeiconspng.com/uploads/circle-png-4.png",
+                iconSize: [7, 7],
+                iconAnchor: [10, 10],
+              })
+            }
+          >
+            <Popup>
+              {
+                station.name // Display location on popup
+                 
+              }
+              {" | Capacity of Dock: "}
+              {
+               station.capacity // Displays capacity of bike dock
+              }
+              
+            </Popup>
+          
+          </Marker>
+        )
+      )}
+    </MapContainer>
+  );
+}
+export default App;
