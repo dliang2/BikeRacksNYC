2a5d6aa8de28ece519a5f81f21de7502eee9f280
Darren Liang <liangdarren2@gmail.com>
2021-11-14 11:36:23 -0500

    changed data to open feed with axios call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import logo from "./logo.svg";
 import React, { useState, useEffect, useContext } from "react";
-
+import axios from "axios";
 import "leaflet/dist/leaflet.css";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { latLngBounds, Icon } from "leaflet";
 
-//import BikeRackData from "./data/BicycleParking.json";
-import BikeRackData from "./data/dummyData.json"; // Using smaller data because main data takes too long to load
-
 import "./App.css";
-// var baseLayer = L.tileLayer('https://maps{s}.nyc.gov/xyz/1.0.0/carto/basemap/{z}/{x}/{y}.jpg', {
 
 function App() {
   // const [latitude, setLatitude] = useState(0);
   // const [longitude, setLongitude] = useState(0);
   // const bounds = latLngBounds([40.0341, -74.2727], [41.2919, -71.9101]);
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://gbfs.citibikenyc.com/gbfs/en/station_information.json")
+      .then((res) => setData(res.data.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     // Generate a world map centered on NYC
     <MapContainer
       center={[40.7128, -74.006]}
-      zoom={10}
+      zoom={11}
       maxZoom={18}
       style={{ width: "100%", height: "900px" }}
       fillColor="grey"
@@ -32,16 +37,15 @@ function App() {
      contributors'
         // bounds={bounds}
       />
-
-      {BikeRackData.data.map(
+      {data.stations?.map(
         (
-          bikerack // Read through the data in the JSON file
+          station // Read through the data in the JSON file
         ) => (
           <Marker // Display every Bike Rack in the data
-            key={bikerack.id}
+            key={station.id}
             position={[
-              bikerack[8].split(" ")[2].slice(0, -1), // get LATITUDE
-              bikerack[8].split(" ")[1].slice(1), // get LONGITUDE
+              station.lat, // get LATITUDE
+              station.lon, // get LONGITUDE
             ]}
             icon={
               // Set image of the marker icon
@@ -49,10 +53,11 @@ function App() {
                 iconUrl:
                   "https://www.freeiconspng.com/uploads/circle-png-4.png",
                 iconSize: [10, 10],
-                // iconAnchor: [10, 10],
               })
             }
-          ></Marker>
+          >
+            <Popup>{station.name}</Popup>
+          </Marker>
         )
       )}
     </MapContainer>
