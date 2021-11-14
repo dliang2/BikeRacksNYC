fd3012b73a68a2314e5a9e8ec5707e95115850b7
Darren Liang <liangdarren2@gmail.com>
2021-11-13 20:52:13 -0500

    changed base map, changed marker icons

--- src/App.js
+++ src/App.js
@@ -4,25 +4,33 @@ import React, { useState, useEffect, useContext } from "react";
 import "leaflet/dist/leaflet.css";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { Icon } from "leaflet";
+import { latLngBounds, Icon } from "leaflet";
 
 //import BikeRackData from "./data/BicycleParking.json";
 import BikeRackData from "./data/dummyData.json"; // Using smaller data because main data takes too long to load
 
 import "./App.css";
+// var baseLayer = L.tileLayer('https://maps{s}.nyc.gov/xyz/1.0.0/carto/basemap/{z}/{x}/{y}.jpg', {
 
 function App() {
   // const [latitude, setLatitude] = useState(0);
   // const [longitude, setLongitude] = useState(0);
-
+  // const bounds = latLngBounds([40.0341, -74.2727], [41.2919, -71.9101]);
   return (
     // Generate a world map centered on NYC
-    <MapContainer center={[40.7128, -74.006]} zoom={10}>
+    <MapContainer
+      center={[40.7128, -74.006]}
+      zoom={10}
+      maxZoom={18}
+      style={{ width: "100%", height: "900px" }}
+      fillColor="grey"
+    >
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
         attribution='&copy;
      <a href="http://osm.org/copyright">OpenStreetMap</a> 
      contributors'
+        // bounds={bounds}
       />
 
       {BikeRackData.data.map(
@@ -39,9 +47,9 @@ function App() {
               // Set image of the marker icon
               new Icon({
                 iconUrl:
-                  "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-                iconSize: [25, 41],
-                iconAnchor: [12, 41],
+                  "https://www.freeiconspng.com/uploads/circle-png-4.png",
+                iconSize: [10, 10],
+                // iconAnchor: [10, 10],
               })
             }
           ></Marker>
